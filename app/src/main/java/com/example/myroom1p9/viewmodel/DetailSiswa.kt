eb59897fb7344f968592952e63a203e4103e9590
IndraHfid <indrahafidsaputra@gmail.com>
2025-12-04 09:21:55 +0700

    Extract idSiswa dan code sebelumnya yang hilang

--- app/src/main/java/com/example/myroom1p9/viewmodel/DetailSiswa.kt
+++ app/src/main/java/com/example/myroom1p9/viewmodel/DetailSiswa.kt
@@ -3,17 +3,19 @@ package com.example.myroom1p9.viewmodel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.myroom1p9.repositori.RepositoriSiswa
+import com.example.myroom1p9.view.route.DestinasiDetailSiswa
 
 class DetailViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoriSiswa: RepositoriSiswa
 ) : ViewModel() {
 
+    private val idSiswa: Int =
+        checkNotNull(savedStateHandle[DestinasiDetailSiswa.itemIdArg])
+
+
 }
 
-/**
- * UI state for ItemDetailsScreen
- */
 data class DetailSiswaUiState(
     val detailSiswa: DetailSiswa = DetailSiswa()
 )
