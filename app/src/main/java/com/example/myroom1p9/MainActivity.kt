4e06e5df20b3b92a0e2f7c7691d9cc945c470ebe
IndraHfid <indrahafidsaputra@gmail.com>
2025-12-04 13:11:06 +0700

    DetailViewModel tanpa ada perubahan dan Ganti SiswaApp ke HostNavigasi dan tambahkan NavController di MainActivity

--- app/src/main/java/com/example/myroom1p9/MainActivity.kt
+++ app/src/main/java/com/example/myroom1p9/MainActivity.kt
@@ -7,12 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.example.myroom1p9.ui.theme.MyRoom1P9Theme
-import com.example.myroom1p9.view.uicontroller.SiswaApp
+import com.example.myroom1p9.view.uicontroller.HostNavigasi
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,13 +20,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyRoom1P9Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SiswaApp(
-                        modifier = Modifier.padding(paddingValues = innerPadding)
+                val navController = rememberNavController()
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    HostNavigasi(
+                        navController = navController,
+                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
     }
 }
-
