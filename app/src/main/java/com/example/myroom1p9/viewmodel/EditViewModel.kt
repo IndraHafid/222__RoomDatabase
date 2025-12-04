261dae319a6004c22eac32bba0f209851035387d
IndraHfid <indrahafidsaputra@gmail.com>
2025-12-04 12:40:31 +0700

    Tambah EditViewModel dasar + state

---
+++ app/src/main/java/com/example/myroom1p9/viewmodel/EditViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.myroom1p9.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.myroom1p9.repositori.RepositoriSiswa
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
