4cbf3c0716b5ea2cdde60714559a1bbf737b6267
IndraHfid <indrahafidsaputra@gmail.com>
2025-12-04 09:32:44 +0700

    ABuat dasar DestinasiDetailSiswa

---
+++ app/src/main/java/com/example/myroom1p9/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,8 @@
+package com.example.myroom1p9.view.route
+
+import com.example.myroom1p9.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+}
