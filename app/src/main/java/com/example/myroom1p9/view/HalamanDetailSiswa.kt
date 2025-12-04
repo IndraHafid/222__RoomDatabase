e492b7833c80735368e792d0f12f820e9c7e848a
IndraHfid <indrahafidsaputra@gmail.com>
2025-12-04 09:42:34 +0700

    tambah struktur awal DetailSiswaScreen dan Scaffold

---
+++ app/src/main/java/com/example/myroom1p9/view/HalamanDetailSiswa.kt
@@ -0,0 +1,30 @@
+package com.example.myroom1p9.view
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myroom1p9.viewmodel.DetailViewModel
+import com.example.myroom1p9.viewmodel.provider.PenyediaViewModel
+import com.example.myroom1p9.view.route.DestinasiDetailSiswa
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiDetailSiswa.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+        floatingActionButton = {
+        }, modifier = modifier
+    ) { innerPadding ->
