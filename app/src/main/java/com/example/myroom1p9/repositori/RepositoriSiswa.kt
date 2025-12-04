0bb7c02f217423bd2b46f7d958cfcc4130b150cb
IndraHfid <indrahafidsaputra@gmail.com>
2025-12-04 12:26:52 +0700

    Menambah fungsi getSiswaStream, updateSiswa, dan deleteSiswa pada RepositoriSiswa

--- app/src/main/java/com/example/myroom1p9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroom1p9/repositori/RepositoriSiswa.kt
@@ -1,22 +1,30 @@
 package com.example.myroom1p9.repositori
 
-// PASTIKAN MENG-IMPORT SISWA DARI 'room'
 import com.example.myroom1p9.room.Siswa
 import com.example.myroom1p9.room.SiswaDao
 import kotlinx.coroutines.flow.Flow
 
-
 interface RepositoriSiswa {
+
     fun getAllSiswaStream(): Flow<List<Siswa>>
+
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+
     suspend fun insertSiswa(siswa: Siswa)
+
+    suspend fun deleteSiswa(siswa: Siswa)
+
+    suspend fun updateSiswa(siswa: Siswa)
 }
 
-class OfflineRepositoriSiswa(
-    private val siswaDao: SiswaDao
-) : RepositoriSiswa {
-
+class OfflineRepositoriSiswa(private val siswaDao: SiswaDao) : RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
 
-    // REVISI: Gunakan tipe Siswa dari 'room' di sini juga
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 }
